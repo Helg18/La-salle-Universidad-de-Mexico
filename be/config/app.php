c946d3b33cdc99fefa1686c8cd5f9ac3c3b47dea
Fernando Castillo MacbookAir <jfcr2204@hotmail.com>
2016-05-11 19:56:54 -0500

    Login template with LaSalle logo

--- be/config/app.php
+++ be/config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class
+
     ],
 
     /*
@@ -201,6 +204,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Shinobi'   => Caffeinated\Shinobi\Facades\Shinobi::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
 
     ],
 

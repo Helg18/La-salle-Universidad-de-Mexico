c09ff19929913b2a6688eb30cd182f92bf40a201
Fernando Castillo MacbookAir <jfcr2204@hotmail.com>
2016-05-31 11:29:25 -0500

    Fix UTF8

--- be/app/Util.php
+++ be/app/Util.php
@@ -105,7 +105,7 @@ class Util{
     static function utf8_converter($array)
     {
         array_walk_recursive($array, function(&$item, $key){
-            if(!mb_detect_encoding($item, 'utf-8', true)){
+            if(is_string($item) && !mb_detect_encoding($item, 'utf-8', true)){
                 $item = utf8_encode($item);
             }
         });

55e81fcac6d272e27fa6479ecceaeacb9a75180b
Fernando Castillo MacbookAir <jfcr2204@hotmail.com>
2016-06-03 09:39:31 -0500

    fix ws.config

--- js/ws.config.js
+++ js/ws.config.js
@@ -1,2 +1,2 @@
-//var url = 'https://secure.kreativeco.com/lasalle/be/public/api/v1/frontend/';
-var url = 'http://localhost:8000/api/v1/frontend/';
+var url = 'https://secure.kreativeco.com/lasalle/be/public/api/v1/frontend/';
+//var url = 'http://localhost:8000/api/v1/frontend/';

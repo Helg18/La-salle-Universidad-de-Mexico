71758f98debe387487fc61f4e290908e26c3c87d
Jean Carlos Nunez <jeancarlosn@hotmail.com>
2016-06-10 20:24:51 -0500

    Link para noticias

--- js/ws.js
+++ js/ws.js
@@ -709,9 +709,9 @@ function noticias(){
         if(con=="" || con==0){
 
             tmpSlices = '<div class="item active">'+
-            '<img src="'+post.picture_url+'" alt="'+post.title+'">'+
+            '<a href="notice.html?id='+post.id+'"> <img src="'+post.picture_url+'" alt="'+post.title+'"></a>'+
             '<div class="carousel-caption">'+
-            '<h3><a href="notice.html?id='+post.id+'">'+post.title+'</a></h3>'+
+            '<h3>'+post.title+'</h3>'+
             '<p>'+post.subtitle+'</p> '+
             '</div></div>';
 

e221a7b98ef84fc0a4f966c93212d567caebda0b
Jean C. Nunez <jeancarlosn2008@gmail.com>
2016-06-24 21:07:03 -0500

    Merge de todos los cambios

--- js/ws.js
+++ js/ws.js
@@ -780,71 +780,10 @@ function lineatiempo(){
     
 }
 
-// function noticias(){
 
-// return false;
-//     $('#ninja-slider ul').html('');
-//     var tmp = ''
-
-
-//     $.each(initial_data.categories[9].posts, function(index,post) {
-
-
-//         tmp = tmp +
-//             '<li>'+
-//             '<a class="ns-img" href="'+post.picture_url+'"></a>'+
-
-//             '<div class="barra_morada">'+
-//             '<div class="caption cap1"><img src="images/nuevosArtes/banner/icono_noticias.png"></div>'+
-//             '<div class="caption cap1 cap2">NOTICIAS</div>'+
-//             '</div>'+
-
-//             '<div class="barra_gris textRight">'+
-//             '<div class="caption cap1gris">'+post.title+'</div>'+
-//             '<div class="caption cap1 cap2gris">'+post.subtitle+'</div>'+
-//             '<div class="caption cap1 cap3"><button  class="btn_slider" data-index="'+index+'"><img src="images/nuevosArtes/banner/icono_mas.png"></button></div>'+
-//             '</div>'+
-//             '</li>';
-
-//             // $("#postdl").append(new Option(post.title,index+'-'+9 ));
-
-
-//     });
-
-
-//     $('#ninja-slider ul').html(tmp);
-
-
-//     nslider.init();
-
-
-//     $("#ninja-slider .btn_slider").on( "click", function() {
-
-//         var index = $(this).data('index'),
-//             post = initial_data.categories[9].posts[index];
-
-//         // console.log(index);
-
-//         $('#desc_slider .title_desc h3').html(post.title);
-//         $('#desc_slider .desc_desc').html(post.paragraph_1_html);
-
-//         if($('#desc_slider').is(":visible") ){
-//             $('#desc_slider').hide("slow");
-//         }else{
-//             $('#desc_slider').show("slow");
-//             $("#changeTextTitulo").text('Noticias');
-//         }
-//     });
-
-
-
-// }
 
 function noticias(){
-
-
-<<<<<<< HEAD
-    
+   
 
     $('.slider3').html(''); 
     txt="";
@@ -898,39 +837,7 @@ function noticias(){
                  }
 
             });
-=======
-    var tmpSlices = '';
-    var tmpindicator = '';
 
-    var con=0;
-
-    $('#myCarousel').append('<ol class="carousel-indicators"></ol>');
-
-    $.each(initial_data.categories[9].posts, function(index,post) {
-
-        if(con=="" || con==0){
-
-
-            tmpindicator = tmpindicator + '<li data-target="#myCarousel" data-slide-to="'+con+'" class="active"></li>';
-            
-             
-            
-            
-            }else{
-            
-          
-
-            tmpindicator = tmpindicator +' <li data-target="#myCarousel" data-slide-to="'+con+'" class=""></li> ';
-
-          
-            
-
-        }
-
-                      
-        
-        con=con+1;
->>>>>>> dd90131c97d68e783df2051e35f388f2a878e4e0
 
         }
 
@@ -952,7 +859,7 @@ function noticias(){
         
     });
 
-<<<<<<< HEAD
+
     $('.slider3').bxSliderPrincipal({});
 
 }
@@ -962,24 +869,8 @@ function AcademicOffer(){
 
     var html    = ''
         parent = this;
-=======
-    $('.carousel-indicators').append(tmpindicator);
 
-    con=0;
 
-    $.each(initial_data.categories[9].posts, function(index,post) {
-
-        if(con=="" || con==0){
->>>>>>> dd90131c97d68e783df2051e35f388f2a878e4e0
-
-            tmpSlices = '<div class="item active">'+
-            '<img src="images/nuevosArtes/banner/banner_dos.png" alt="'+post.title+'">'+
-            '<div class="carousel-caption">'+
-            '<h3>'+post.title+'</h3>'+
-            '<p>'+post.subtitle+'</p> '+
-            '</div></div>';
-
-<<<<<<< HEAD
     this.showContent =  function(index,is_showed){
 
         $('#container_menu_derecho').hide();
@@ -1088,53 +979,6 @@ function AcademicOffer(){
                AO.showSidebar($(this).val());
             });
 
-=======
-            $('.carousel-inner').append(tmpSlices);  
-
-            
-            }else{
-            
-            tmpSlices = '<div class="item ">'+
-            '<img src="images/nuevosArtes/banner/banner_dos.png" alt="'+post.title+'">'+
-            '<div class="carousel-caption">'+
-            '<h3>'+post.title+'</h3>'+
-            '<p>'+post.subtitle+'</p> '+
-            '</div></div>';
-
-            
-
-            $('.carousel-inner').append(tmpSlices);  
-            
-
-        }
-
-                      
-        
-        con=con+1;
-
-    });
->>>>>>> dd90131c97d68e783df2051e35f388f2a878e4e0
-
-    
-    
-
-      
-    control = '<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">'+
-    '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'+
-    '<span class="sr-only">Previous</span>'+
-    '</a>'+
-    '<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">'+
-    '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'+
-    '<span class="sr-only">Next</span>'+
-    '</a>';
-
-
-    $('#myCarousel').append(control);        
-
-
-   
-
-    
 
 
             //Mostrar/Ocultar el listbox y el menu
@@ -1202,7 +1046,6 @@ function AcademicOffer(){
 
 
 
-<<<<<<< HEAD
 function getUrlVars()
   {
       var vars = [], hash;
@@ -1215,5 +1058,3 @@ function getUrlVars()
       }
       return vars;
   }
-=======
->>>>>>> dd90131c97d68e783df2051e35f388f2a878e4e0

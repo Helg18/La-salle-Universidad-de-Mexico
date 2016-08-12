875449c2c6999f9b49771f44de287ca8ea7babfe
Jean C. Nunez <jeancarlosn2008@gmail.com>
2016-08-12 15:21:49 -0500

    Cambios en la manera de colocar las subsubcategorias

--- js/centros.js
+++ js/centros.js
@@ -14,43 +14,45 @@ function getInitialData(lang,id){
         if(lang==1 || lang=="" || lang==undefined){ruta = 'initial-data';}else{
             ruta = 'initial-data-e';
         }
-
+        con=1;
         $.get( urlc + ruta +'/'+id, function(data){
 
             initial_data = data;
-            
 
             $('#principal').html("<div id='conten' class='efectos-centro-info effects clearfix'></div>");
             html="";
              $.each(initial_data.subcategorias, function(index,subcategorias) {
                 
                 html = "<div class='col-sm-12 encabezados' >"+subcategorias.title+"</div>";
-                html = html + "<div id='imgnot"+index+"' class='imgNoticia'><div>";
                 $('#conten').append(html);
-                $.each(initial_data.subsubcategorias, function(index,subsubcategorias) {
 
-                    html = "<div class='overlayTituloNoticia'><h6>"+subsubcategorias.subtitle+"</h6></div>";
-                    html = html + "<img src='../images/nuevosArtes/Noticias/image_news1.png' alt=''>"; 
-                    html = html + "<div class='overlayNoticia'><h6>"+subsubcategorias.paragraph_1+"</h6><a href='editoriales.html' id='' class='btn_noticia_more'><img src='../images/nuevosArtes/Noticias/icon_more.png' class='encabezadosimg'></a></div>";
-                    $('#imgnot'+index).append(html);
+                
+                             
+                $.each(initial_data.subsubcategorias, function(index2,subsubcategorias) {
+                    html='';
+                    
+
+                    if(subsubcategorias.id_sub_categoria == subcategorias.id)
+                    {
+
+                      html = html + "<div id='imgnot"+con+"' class='imgNoticia'><div>";
+                      $('#conten').append(html); 
+
+                      html = "<div class='overlayTituloNoticia'><h6>"+subsubcategorias.subtitle+"</h6></div>";
+                      html = html + "<img src='../be/public/images/centro/"+subsubcategorias.picture+"' alt=''>"; 
+                      html = html + "<div class='overlayNoticia'><h6>"+subsubcategorias.paragraph_1+"</h6><a href='editoriales.html' id='' class='btn_noticia_more'><img src='../images/nuevosArtes/Noticias/icon_more.png' class='encabezadosimg'></a></div>";
+                      $('#imgnot'+con).append(html);
+                      con=con+1;   
+                    }
+                    
 
                 });
                 
+             
              });
-
-              // $('#principal').html(html);
-
-
 
         });
     }
-
-
-// $("#imgnot0").mouseover(function() 
-//   {
-//     $( this ).addClass( 'imgNoticia hoverNoticia' );
-//   }
-//   );
 
 
 function getUrlVars()
